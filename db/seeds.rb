4d8ea3243ea9577e8d43c0a86347c0faa13e9b99
jbratton <jeremy@alumni.rice.edu>
2015-03-10 15:31:04 -0500

    add seed Carriers and Plans

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Plan.create([{ name: 'Free' }, { name: 'Paid' }])
+
+Carrier.create(name: 'AT&T', sms_gateway_host: 'txt.att.net')
+Carrier.create(name: 'Sprint', sms_gateway_host: 'messaging.sprintpcs.com')
+Carrier.create(name: 'Verizon', sms_gateway_host: 'vtext.com')
+Carrier.create(name: 'T-Mobile', sms_gateway_host: 'tmomail.net')
+
