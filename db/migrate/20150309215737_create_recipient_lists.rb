0ed6a69202e5adbfbbdbf5f491967fe84fab644c
jbratton <jeremy@alumni.rice.edu>
2015-03-10 16:15:54 -0500

    add type column for GlobalList single table inheritance

--- db/migrate/20150309215737_create_recipient_lists.rb
+++ db/migrate/20150309215737_create_recipient_lists.rb
@@ -2,6 +2,7 @@ class CreateRecipientLists < ActiveRecord::Migration
   def change
     create_table :recipient_lists do |t|
       t.string :name
+      t.string :type
       t.references :organization
 
       t.timestamps null: false
